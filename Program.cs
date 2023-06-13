38ff79e65bf0bdff559bddb078aa8b9da07bf51a
Bournane Mohamed <mbournane@gmail.com>
2023-06-13 17:44:10 +0200

    Quest Lists

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+namespace Lists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> strCollection = new List<string>();
+        strCollection.Add("Lion");
+        strCollection.Add("Lapin");
+        strCollection.Add("Elephant");
+
+        strCollection.RemoveAt(0);
+        strCollection.Remove("Elephant");
+        strCollection[0]= "Chat";
+
+        foreach(var str in strCollection)
+            Console.WriteLine(str);        
+    }
+}
